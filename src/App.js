585f188842518ac603f31fe282326fbfab4fa344
Sam Parsons <samparsons269@gmail.com>
2018-12-24 19:24:20 -0500

    merry chistmans

--- src/App.js
+++ src/App.js
@@ -1,18 +1,50 @@
 import React, { PureComponent } from 'react';
 import { View, Button } from 'react-native';
-import Video from 'react-native-video';
+// import Video from 'react-native-video';
 import SquareTicker from './SquareTicker';
-import woodblockSound from './woodblock.wav';
+// import woodblockSound from './woodblock.wav';
 import styles from './App.style';
+import Sound from 'react-native-sound';
 
 
 export default class App extends PureComponent {
-  state = {
-    playing: false,
-  };
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      soundSrc: undefined,
+      playing: false,
+      soundPath: 'woodblock.wav',
+      intervalID: undefined
+    }
+  }
+
+  componentWillMount() {
+    Sound.setCategory('Playback');
+    var wbSound = new Sound(this.state.soundPath, Sound.MAIN_BUNDLE, (error) => {
+      if (error) {
+        console.log('failed to load sound', error);
+        return;
+      }
+      // successful load
+      console.log("duration in seconds: " + wbSound.getDuration() + ' number of channels: ' + wbSound.getNumberOfChannels());
+    });
+    this.setState({ soundSrc: wbSound })
+  }
+
+  // multiple setState philosophy?
   togglePlaying = () => {
-    Video.
+    var tempSound = this.state.soundSrc; // use wbSound for this name or too confusing?
+    if (!this.state.playing) {
+      var intervalID = setInterval(() => {
+        tempSound.play()
+      }, 500);
+      this.setState({ intervalID: intervalID }); // how to properly handle these setStates
+    } else {
+      clearInterval(this.state.intervalID);
+      tempSound.stop() // this may not be necessary
+      this.setState({ intervalID: null }); // possible to combine with lower setState?
+    }
     this.setState(priorState => ({
       playing: !priorState.playing,
     }));
@@ -25,7 +57,7 @@ export default class App extends PureComponent {
         title={ this.state.playing ? 'stop' : 'start' }
       />
       <SquareTicker playing={ this.state.playing } />
-      <Video source={ woodblockSound } />
+      {/* <Video source={ woodblockSound } /> */}
     </View>
   );
 }
